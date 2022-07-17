6f8c1e89fbadab49c47cb0ce442900b9ab379953
ndr_brt <andrea.bertagnolli@gmail.com>
2022-07-17 14:46:41 +0200

    Fix boottidal path

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,10 +12,16 @@ use crate::tidal::Tidal;
 struct MyState(Mutex<Tidal>);
 
 #[tauri::command]
-fn tidal_eval(state: tauri::State<MyState>, window: Window, code: String) {
+fn tidal_eval(state: tauri::State<MyState>, window: Window, code: String, app_handle: tauri::AppHandle) {
   let mut tidal = state.0.lock().unwrap();
+  
   if !tidal.is_running() {
-    tidal.start(window, "./resources/BootTidal.hs".to_string())
+    let mut resource_dir = app_handle.path_resolver().resource_dir().expect("Error getting resource directory path");
+    resource_dir.push("resources");
+    resource_dir.push("BootTidal.hs");
+    let path = resource_dir.to_str().expect("Error getting BootTidal.hs path").to_string();
+    println!("{}", path);
+    tidal.start(window, path);
   }
   tidal.send_line(code.to_string());
 }
@@ -30,7 +36,7 @@ fn main() {
     .menu(tauri::Menu::os_default(&context.package_info().name))
     .build(context)
     .expect("error while running tauri application");
-
+  
   app.run(|_app_handle, event| match event {
     tauri::RunEvent::ExitRequested { api, .. } => {
       api.prevent_exit();
